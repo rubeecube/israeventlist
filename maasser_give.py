d34187fa3ef784d4e6017344526cb5a7a5c377bb
Ruben <ruben@bokobza.info>
2024-06-02 17:37:04 +0300

    Fix parsing?

--- maasser_give.py
+++ maasser_give.py
@@ -63,12 +63,9 @@ def fun_maasser_give_annex_date(update: Update, context: CallbackContext, return
     print(date)
     message.delete()
 
-    try:
-        context.chat_data['ADD']['parsed_date'] = parse_date(date)
-        context.chat_data['ADD']['original_date'] = date
-    except (TypeError, ValueError):
-        send_message('MASR: error', update, context)
-        return MAASSER_NOMINAL
+    context.chat_data['ADD']['parsed_date'] = parse_date(date)
+    context.chat_data['ADD']['original_date'] = date
+
 
     send_message(localize("MASR: amount--format", get_lang(update))
                  .format(MaasserCurrency.currency_to_str(maasser_user.currency)),
